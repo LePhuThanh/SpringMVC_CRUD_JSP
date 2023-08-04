bf1c624f889ac0f0ed6294704cf7c0b26de545ed
LePhuThanh <lpthanh710@gmail.com>
2023-08-04 17:04:58 +0700

    init & fixbug java 19 project

---
+++ src/main/java/com/springmvc_crud/springmvc_crud/SpringmvcCrudApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc_crud.springmvc_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc_crud/springmvc_crud/SpringmvcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc_crud.springmvc_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
